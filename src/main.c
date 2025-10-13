624884a74dfdba5fc6aeeb2f908eb8893303e3fe
Aaron Collins <aaron.collins3@gmail.com>
2025-10-13 06:50:40 -0500

    fixing

--- src/main.c
+++ src/main.c
@@ -1,4 +1,68 @@
 #include <windows.h>
+#include <stdbool.h>
+
+// Rename static for different use cases
+#define global_variable static
+#define local_persist static
+#define internal_function static
+
+// TODO: global for now
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+
+// WIN32 prefix on non-msdn functions
+
+void WIN32ResizeDIBSection(HDC DeviceContext, int Width, int Height)
+{
+    // TODO: Maybe don't free first, free after, then free first if that fails 
+    // TODO: Free DIBSection
+
+    if(BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+
+    //
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+    BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
+    BitmapInfo.bmiHeader.biClrUsed = 0;
+    BitmapInfo.bmiHeader.biClrImportant = 0;
+
+    // Create back-buffer for the Window
+    BitmapHandle = CreateDIBSection(
+        DeviceContext, &BitmapInfo,
+        DIB_RGB_COLORS,
+        // Memory back from Windows to draw into
+        &BitmapMemory, 
+        0, 0);
+
+}
+
+void WIN32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    const VOID *lpBits;
+    const BITMAPINFO *lpBitsInfo;
+    // Rectangle copy for scaling
+    int StretchDIBits(
+        DeviceContext,
+        // (Destination) Bbt Part of the window that needs to be redwrawn
+        X, Y, Width, Height,
+        // (Source)
+        X, Y, Width, Height,
+        lpBits,
+        lpBitsInfo,
+        DIB_RGB_COLORS, // DIB_PAL_COLORS
+        // Replace and copy the window directly
+        SRCCOPY);
+}
 
 LRESULT CALLBACK
 MainWinCallback(HWND Window,
@@ -14,18 +78,28 @@ MainWinCallback(HWND Window,
     {
         // User resized the window
         OutputDebugString("WM_SIZE\n");
+
+        RECT ClientRect;
+        GetClinetRect(Window, &ClientRect);
+
+        int Height = ClientRect.bottom - ClientRect.top;
+        int Width = ClientRect.right - ClientRect.left;
+
+        WIN32ResizeDIBSection(Width, Height);
     }
     break;
     case WM_DESTROY:
     {
         // User deletes the window
         OutputDebugString("WM_DESTROY\n");
+        Running = false;
     }
     break;
     case WM_CLOSE:
     {
         // User clicked close
         OutputDebugString("WM_CLOSE\n");
+        Running = false;
     }
     break;
     case WM_ACTIVATEAPP:
@@ -46,9 +120,7 @@ MainWinCallback(HWND Window,
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 
-        // Paint a white background
-        PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
-
+        WIN32UpdateWindow(Window, X, Y, Width, Height);
         EndPaint(Window, &Paint);
     }
     default:
@@ -67,8 +139,6 @@ WinMain(HINSTANCE Instance,
         LPSTR CommandLine,
         int ShowCode)
 {
-    // MessageBox(0, "This is c_render.", "c_render", MB_OK|MB_ICONINFORMATION);
-
     WNDCLASS WindowClass = {};
 
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
@@ -96,12 +166,14 @@ WinMain(HINSTANCE Instance,
             );
         if (WindowHandle)
         {
+            Running = true;
+
             // Handle message queue
-            MSG Message;
-            for(;;)
+            while (Running)
             {
-                BOOL MessageResult = GetMessage(&Message, 0,0,0);
-                if(MessageResult > 0)
+                MSG Message;
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0)
                 {
                     // Parsing for keyboard input
                     TranslateMessage(&Message);
