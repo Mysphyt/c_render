f267f2a5d8ca86e23a00832b9b61b6a93cc71538
Aaron Collins <aaron.collins3@gmail.com>
2025-10-13 18:44:01 -0500

    moving colors

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 // Rename static for different use cases
 #define global_variable static
@@ -9,58 +10,114 @@
 // TODO: global for now
 global_variable bool Running;
 global_variable BITMAPINFO BitmapInfo;
-global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
+global_variable void *BitmapMemory; // C does not understand what VOID* points to
+global_variable int BitmapHeight;
+global_variable int BitmapWidth;
+global_variable int BytesPerPixel = 4;
+
+// Define useful types
+// . (u)int(n)_t is the correct size regardless of platform
+typedef uint8_t uint8;
+typedef uint32_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+void
+RenderGradient(int XOffset, int YOffset)
+{
+    // Row width in bytes
+    int Pitch = BitmapWidth*BytesPerPixel;
+
+    // Strides may not match pixel boundry
+    uint8 *Row = (uint8 *)BitmapMemory;
+    // Loop through each bit in the Bitmap
+    for (int Y = 0;
+         Y < BitmapHeight;
+         ++Y)
+    {
+        uint32 *Pixel = (uint32 *)Row;
+        // Incriment each pixel to make sure pitch is aligned
+        for (int X = 0;
+             X < BitmapWidth;
+             ++X)
+        {
+            /*
+                                   0  1  2  3
+                Pixel in memory:  BB GG RR xx
+            */
+
+            uint8 Blue = (X + XOffset);
+            uint8 Green = (Y + YOffset);
+            
+            /*
+                Memory:     BB GG RR xx            
+                Register:   xx RR GG BB
+            */
+            *Pixel++ = ((Green << 8) | Blue);
+        }
+        Row += Pitch;
+    }
+}
 
 // WIN32 prefix on non-msdn functions
-
-void WIN32ResizeDIBSection(HDC DeviceContext, int Width, int Height)
+void 
+WIN32ResizeDIBSection(int Width, int Height)
 {
-    // TODO: Maybe don't free first, free after, then free first if that fails 
-    // TODO: Free DIBSection
-
-    if(BitmapHandle)
+    if (BitmapMemory)
     {
-        DeleteObject(BitmapHandle);
+        // MEM_RELEASE instead of MEM_DECOMMIT because RELEASE actually frees as well, use DECOMMIT if you want them back later
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
     }
+    // Width and height in pixels
+    BitmapHeight = Height;
+    BitmapWidth = Width;
 
     //
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
+
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    // Negative BitmapHeight for top-down indexing
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
+
+    // 1 plane
     BitmapInfo.bmiHeader.biPlanes = 1;
+    // 32 bits per pixe (8x3-rgb + 8-padding), align pixels on 4-byte boundaries
     BitmapInfo.bmiHeader.biBitCount = 32;
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
-    BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
-    BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
-    BitmapInfo.bmiHeader.biClrUsed = 0;
-    BitmapInfo.bmiHeader.biClrImportant = 0;
 
-    // Create back-buffer for the Window
-    BitmapHandle = CreateDIBSection(
-        DeviceContext, &BitmapInfo,
-        DIB_RGB_COLORS,
-        // Memory back from Windows to draw into
-        &BitmapMemory, 
-        0, 0);
+    int BytesPerPixel = 4;
+    int BitmapMemorySize = (Width * Height) * BytesPerPixel;
 
+    //
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
-void WIN32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+void 
+WIN32UpdateWindow(HDC DeviceContext, RECT *ClientRect, int X, int Y, int Width, int Height)
 {
-    const VOID *lpBits;
-    const BITMAPINFO *lpBitsInfo;
-    // Rectangle copy for scaling
-    int StretchDIBits(
+    /*
+        Bitmaps stored in "1D" memory translate to "2D" using "pitch" and "stride"
+        ... need to know which one StretchDIBits uses
+    */
+    int WindowWidth = ClientRect->right - ClientRect->left;
+    int WindowHeight = ClientRect->bottom - ClientRect->top;
+
+    // Update the desplay by drawing to a destination rectangle
+    // . Write the Bitmap to the Window
+    StretchDIBits(
         DeviceContext,
-        // (Destination) Bbt Part of the window that needs to be redwrawn
-        X, Y, Width, Height,
-        // (Source)
-        X, Y, Width, Height,
-        lpBits,
-        lpBitsInfo,
-        DIB_RGB_COLORS, // DIB_PAL_COLORS
-        // Replace and copy the window directly
+        0, 0, BitmapWidth, BitmapHeight,
+        0, 0, WindowWidth, WindowHeight,
+        BitmapMemory,
+        &BitmapInfo,
+        // Use RGB colors
+        DIB_RGB_COLORS,
+        // Copy the bitmap directly
         SRCCOPY);
 }
 
@@ -80,7 +137,7 @@ MainWinCallback(HWND Window,
         OutputDebugString("WM_SIZE\n");
 
         RECT ClientRect;
-        GetClinetRect(Window, &ClientRect);
+        GetClientRect(Window, &ClientRect);
 
         int Height = ClientRect.bottom - ClientRect.top;
         int Width = ClientRect.right - ClientRect.left;
@@ -113,6 +170,9 @@ MainWinCallback(HWND Window,
         // User paint request
         PAINTSTRUCT Paint;
 
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);
+
         HDC DeviceContext = BeginPaint(Window, &Paint);
 
         int X = Paint.rcPaint.left;
@@ -120,7 +180,8 @@ MainWinCallback(HWND Window,
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 
-        WIN32UpdateWindow(Window, X, Y, Width, Height);
+        WIN32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
+
         EndPaint(Window, &Paint);
     }
     default:
@@ -149,7 +210,7 @@ WinMain(HINSTANCE Instance,
 
     if (RegisterClass(&WindowClass))
     {
-        HWND WindowHandle =
+        HWND Window =
             CreateWindowEx(
                 0,                                // DWORD dwExStyle
                 WindowClass.lpszClassName,        // LPCTSTR lpClassName
@@ -164,25 +225,46 @@ WinMain(HINSTANCE Instance,
                 Instance,                         // HINSTANCE hInstance
                 0                                 // LPVOID lpParam
             );
-        if (WindowHandle)
+        if (Window)
         {
             Running = true;
+
+            int XOffset = 0;
+            int YOffset = 0;
 
             // Handle message queue
             while (Running)
             {
                 MSG Message;
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                if (MessageResult > 0)
+
+                // PeekMessage does not block when there is no message
+                // . PM_REMOVE, remove the message from the queue
+                while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
                 {
+                    if(Message.message == WM_QUIT)
+                    {
+                        Running = false;
+                    }
+
                     // Parsing for keyboard input
                     TranslateMessage(&Message);
                     DispatchMessage(&Message);
                 }
-                else
-                {
-                    break;
-                }
+
+                RenderGradient(XOffset, YOffset);
+
+                RECT ClientRect;
+                GetClientRect(Window, &ClientRect);
+
+                HDC DeviceContext = GetDC(Window);
+
+                int WindowWidth = ClientRect.right - ClientRect.left;
+                int WindowHeight = ClientRect.bottom - ClientRect.top;
+                WIN32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+
+                ReleaseDC(Window, DeviceContext);
+
+                ++XOffset;
             }
         }
         else
